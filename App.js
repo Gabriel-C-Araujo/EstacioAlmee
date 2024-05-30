f27cd502e9569ceb2b30e4e900996306ad6b8461
Nicholas "nilt'" Leite <nicklleite@gmail.com>
2024-05-30 01:08:15 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {Calendar, LocaleConfig} from 'react-native-calendars';
+import { Button } from 'react-native-web';
 
 export default function App() {
   return (
@@ -9,11 +11,12 @@ export default function App() {
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
